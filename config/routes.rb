04888b9f8837526440e1d2e7c0204d4fd3171ee1
Leo <leo.decandia@gmail.com>
2012-04-22 14:12:24 -1000

    added login and logout.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,19 @@
 Depot::Application.routes.draw do
+
+  get 'admin' => 'admin#index'
+
+  controller :sessions do
+    get 'login' => :new
+    post 'login' => :create
+    delete 'logout' => :destroy
+  end
+
+  #made by generate
+  #get "admin/index"
+  #get "sessions/new"
+  #get "sessions/create"
+  #get "sessions/destroy"
+
   resources :users
 
   resources :orders
